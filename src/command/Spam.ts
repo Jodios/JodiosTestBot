16654b3674d45f68579c916fbc6f929dcc1858e1
Joel Ortiz <joel.ortiz1@aa.com>
2020-08-18 20:26:20 -0500

    slight changes

--- src/command/Spam.ts
+++ src/command/Spam.ts
@@ -4,22 +4,19 @@ export async function spam(user: Discord.User){
 
     console.log(`sending a message to ${user}`);
     for (let i = 0; i < 100; i++){
-        user.send("hi");
+        user.send("HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO").catch(onError);
         await pause(1);
     }
 
 }
 
-function sendMessage(user: Discord.User){
-    user.send("hi");
-}
-
 function onError(err: Error){
     console.log(err.message);
 }
-
+ 
 async function pause(sec: number){
     return new Promise( (resolve, rejcet) => setTimeout(resolve, sec * 1000) );
 }
+
 
 
--- src/service/PokeNerdService.ts
+++ src/service/PokeNerdService.ts
@@ -4,32 +4,38 @@ import axios from "axios";
 const url = "https://pokeapi.co/api/v2/pokemon/";
 let time = new Date().getTime();
 let actualName = "";
+const insults: string[] = ["You fucking nerd.", "What a nerd"];
 
 export function PokeNerdService(n: number, channel: Discord.TextChannel) {
-    if ( 45 < n && n < 55){
-
-        let pokeURL = `${url}${Math.floor(Math.random() * 806 + 1)}`;
-        axios.get(pokeURL).then(res => {
-            time = new Date().getTime();
-            actualName = res.data.forms[0].name;
-            let image = res.data.sprites.front_default;
-            channel.send("Who's that pokemon?", { files: [image] });
-            console.log(actualName);
-    
-        }).catch(err => onFailed(err, pokeURL));
-
-    }
+  if (45 < n && n < 55) {
+    let pokeURL = `${url}${Math.floor(Math.random() * 806 + 1)}`;
+    axios
+      .get(pokeURL)
+      .then((res) => {
+        time = new Date().getTime();
+        actualName = res.data.forms[0].name;
+        let image = res.data.sprites.front_default;
+        channel.send("Who's that pokemon?", { files: [image] });
+        console.log(actualName);
+      })
+      .catch((err) => onFailed(err, pokeURL));
+  }
 }
 
-export function guessName(guess: string, channel: Discord.TextChannel, user: string) {
-    let sec: number = ((new Date().getTime()) - time) / 1000;
-    let min: number = sec / 60;
-    if (min < 1 && ( actualName.toLowerCase() ) == guess.toLowerCase() ){
-        channel.send(`<@${user}> you fucking nerd` );
-    }
+export function guessName(
+  guess: string,
+  channel: Discord.TextChannel,
+  user: string
+) {
+  let sec: number = (new Date().getTime() - time) / 1000;
+  let min: number = sec / 60;
+  if (min < 1 && actualName.toLowerCase() == guess.toLowerCase()) {
+    let rn = Math.ceil(Math.random() * insults.length - 1);
+    channel.send(`<@${user}> ${insults[rn]}`);
+  }
 }
 
 function onFailed(err: Error, url: string) {
-    console.log(err.message);
-    console.log(url);
+  console.log(err.message);
+  console.log(url);
 }
