26d90b221088df0d7a80ad7688dbbe1260bd5d2c
Joel Ortiz <jortiz@pop-os.localdomain>
2020-08-23 15:09:13 -0500

    Added Clear command

--- src/command/Clear.ts
+++ src/command/Clear.ts
@@ -1,16 +1,21 @@
 import Discord from "discord.js";
 
-export async function clear(channel: Discord.TextChannel, user: Discord.GuildMember){
-    
-    if (user.hasPermission("ADMINISTRATOR")){
-        channel.send(`${user.nickname} is an Admin`);
-    }else{
-        channel.send(`${user.nickname} is a normal`);
+export async function clear(channel: Discord.TextChannel, user: Discord.GuildMember) {
+
+    if (user.hasPermission("ADMINISTRATOR")) {
+        channel.messages.fetch().then((messages: Discord.Collection<string, Discord.Message>) => {
+            messages.forEach((message: Discord.Message) => {
+                message.delete().catch(onError);
+            })
+            channel.send(`${user.nickname == null ? user.displayName : user.nickname} has deleted all messages.`);
+        })
+    } else {
+        channel.send(`${user.nickname == null ? user.displayName : user.nickname} doesn't have permissions to delete messages.`);
     }
 
 }
 
-function onError(err: Error){
+function onError(err: Error) {
     console.log(err.message);
 }
 
