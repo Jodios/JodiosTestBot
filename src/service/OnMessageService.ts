a4602799fd6877748a77ffc14ba43002db3cb035
Joel Ortiz <jortiz@Joels-MacBook-Air.local>
2020-11-25 14:56:47 -0600

    removing features i have no plans for

--- src/service/OnMessageService.ts
+++ src/service/OnMessageService.ts
@@ -2,10 +2,7 @@ import Discord from "discord.js";
 import { mock } from "../command/Mock";
 import { insult } from "../command/Insult";
 import { greentext } from "../command/Greentext";
-import { crypto } from "../command/Crypto";
-import { spam } from "../command/Spam";
 import { clear } from "../command/Clear";
-import { holyAssembly } from "../command/HolyAssembly";
 import { PokeNerdService as pokeNerd, guessName } from "../service/PokeNerdService";
 
 export function onMessage(client: Discord.Client) {
@@ -31,18 +28,9 @@ export function onMessage(client: Discord.Client) {
                 case 'greentext':
                     greentext((msg.channel as Discord.TextChannel));
                     break;
-                case 'crypto':
-                    crypto((msg.channel as Discord.TextChannel), args);
-                    break;
-                case 'spam':
-                    spam( msg.mentions.users.array()[0], args.join(" ") );
-                    break;
                 case 'clear':
                     clear((msg.channel as Discord.TextChannel), (msg.member as Discord.GuildMember));
-                    break;                
-                // case 'assembly':
-                //     holyAssembly( args.join(" "), (msg.channel as Discord.TextChannel), (msg.member as Discord.GuildMember));
-                //     break;
+                    break;
             }
         }
     });
