fa0651499485a21aeb8130ab8979251f24e8bfb6
jodios <joelandapophis@gmail.com>
2021-06-02 22:30:06 -0500

    can't have conversations now

--- src/main/service/OnVoiceChangeService.ts
+++ src/main/service/OnVoiceChangeService.ts
@@ -1,40 +1,23 @@
 import Discord, { VoiceState, VoiceChannel } from "discord.js";
-import ytdl from "ytdl-core"
+import path from "path";
 
 const joinn = ["https://www.youtube.com/watch?v=ybGOT4d2Hs8"];
 const leavee = ['https://www.youtube.com/watch?v=5r06heQ5HsI','https://www.youtube.com/watch?v=XsoNZ_RfJSk'];
+const basePath = path.join(__dirname, "../resources");
+//@ts-ignore
+let player: Discord.StreamDispatcher = undefined;
 
-export function onVoiceChange(client: Discord.Client){
+export const onVoiceChange = (client: Discord.Client, connection: Discord.VoiceConnection) => {
 
-    client.on("voiceStateUpdate", (oldState: Discord.VoiceState, newState: Discord.VoiceState) => {
+    client.on("guildMemberSpeaking", (user, speaking) => {
+        let x = 2; 
+        if(speaking.bitfield == 1){
+            player = connection.play(path.join(basePath, "ree.mp3"), {"volume": 1});
+        }else{
+            if(player){
+                 player.end();
+            }
+        } 
+    });
 
-        let botMadeChange: boolean = newState.member?.user.username == client.user?.username;
-        let mute: boolean = (newState.selfMute! != oldState.selfMute!) || (newState.mute! != oldState.mute!);
-        let rnjoin = Math.floor(Math.random() * joinn.length)
-        let rnleave = Math.floor(Math.random() * leavee.length)
-
-        if( newState.channel?.members.size == undefined && !botMadeChange){
-            playTheme(oldState.channel as Discord.VoiceChannel, leavee[rnleave]);
-        }else if(!botMadeChange && !mute){
-            playTheme(newState.channel as Discord.VoiceChannel, joinn[rnjoin]);
-        }
-
-    })
-
-}
-
-async function playTheme(voiceChannel: Discord.VoiceChannel, soundUrl: string){
-
-    try {
-        let connection = await voiceChannel.join();
-    
-        let dispatcher =  connection.play(ytdl(soundUrl, { filter: 'audioonly' } ))
-        dispatcher.on('finish', () => {
-            voiceChannel.leave();
-            dispatcher.destroy();
-        });
-    } catch (error) {
-        console.log(error);
-    }
-    
 }
