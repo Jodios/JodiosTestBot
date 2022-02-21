c8bdb668d39793cd28a78ab561d0ec0d2582c7d4
Jim Knox <knoxiiw@gmail.com>
2022-02-21 18:01:15 -0500

    Merge branch 'greentext-404-fix' of https://github.com/Jodios/JodiosTestBot into greentext-404-fix

--- src/main/command/Greentext.ts
+++ src/main/command/Greentext.ts
@@ -1,62 +1,69 @@
 import Discord from "discord.js";
 import axios, { AxiosResponse } from "axios";
 import { Storage } from "firebase-admin/storage";
-import { StorageReference, uploadBytes, ref } from "firebase/storage";
-import { metadata } from "figlet";
 
 var url = `https://api.pushshift.io/reddit/search/submission/?subreddit=greentext&sort=desc&sort_type=created_utc&size=1000`;
+
+class Greentext {
+    comment: string = '';
+    fileName: string = '';
+    buffer: any = null;
+}
 
 /**
  * greentext is called from @OnMessageService 
  * Sends a request to reddit for /r/greentext
  * @param channel 
+ * @param storage 
+ * @param allowedRetries the number of times we're allowed to retry posting a greentext
  */
-export async function greentext(channel: Discord.TextChannel, storage: Storage) {
-    let retriesAllowed = 3;
-    axios.get(url).then(res => onSuccess(res, channel, storage, retriesAllowed)).catch(err => onFailed(err.message, channel));
+export async function greentext(channel: Discord.TextChannel, storage: Storage, allowedRetries: number) {
+    if (!allowedRetries) {
+        onFailed("Ran out of retries for greentext command. ", channel);
+        return;
+    }
+    axios.get(url).then((res) => {
+        getRandomGreentext(res.data.data).then((res: Greentext) => {
+            let file = storage.bucket().file(`jodiostestbot/greentext/${res.fileName}`);
+            file.save(res.buffer).then(async() => {
+                await file.makePublic();
+                channel.send(res.comment, new Discord.MessageAttachment(res.buffer, res.fileName));
+            }).catch(err => onFailed(err.message, channel));
+        }).catch(err => {
+            if (err.response.status == 404)
+                greentext(channel, storage, allowedRetries--);
+            else
+                onFailed(err.message, channel);
+        });
+    }).catch(err => onFailed(err.message, channel));
 }
 
 /**
- * onSuccess invoked by greentext on successful request
+ * invoked by greentext on successful request
  * to reddit. Generates a random number based on the length
  * of posts provided by reddit and returns that picture.
- * @param response 
- * @param channel 
- * @param storage 
- * @param retriesRemaining number of times onSuccess is allowed to retry posting a random greentext image
+ * @param data 
  */
-async function onSuccess(response: AxiosResponse, channel: Discord.TextChannel, storage: Storage, retriesRemaining: number) {
-    let data = response.data.data;
-    let urls: { link: string, comment: string }[] = data.map((post: any) => { return { link: post?.url, comment: post?.title } });
-    urls = urls.filter(u => u.link.includes("i.redd.it"));
-    let rn = Math.ceil(Math.random() * urls.length - 1);
-    let randomUrl = urls[rn]['link'];
-    let comment = urls[rn]['comment'];
+function getRandomGreentext(data: any): Promise<Greentext> {
+    return new Promise((resolve, reject) => {
+        let greentextResult = new Greentext;
+        let urls: { link: string, comment: string }[] = data.map((post: any) => { return { link: post?.url, comment: post?.title } });
+        urls = urls.filter(u => u.link.includes("i.redd.it"));
 
-    let extension = randomUrl.split("\/").filter((val, index) => val !== "")[2].split(".")[1];
-    let name = Math.floor(new Date().getTime() / 1000);
-    let bucket = storage.bucket();
-    // let reference: StorageReference = ref(storage, `/jodiostestbot/greentext/${name}.${extension}`);
+        let randomNumber = Math.ceil(Math.random() * urls.length - 1);
+        let randomUrl = urls[randomNumber]['link'];
+        greentextResult.comment = urls[randomNumber]['comment'];
 
-    console.log(`Getting image from: ${randomUrl}`);
-    axios.get(randomUrl, { responseType: 'arraybuffer' }).then(res => {
-        let buffer = Buffer.from(res.data, "utf-8");
-        let file = bucket.file(`jodiostestbot/greentext/${name}.${extension}`);
-        file.save(buffer).then(async() => {
-            await file.makePublic()
-            let attachement = new Discord.MessageAttachment(buffer, `${name}.${extension}`)
-            channel.send(comment, attachement);
-        }).catch(err => onFailed(err.message, channel));
-    }).catch((err) => {
-        if (err.response.status == 404) {
-            if (retriesRemaining--) {
-                onSuccess(response, channel, storage, retriesRemaining);
-            } else {
-                onFailed("Ran out of retries when getting greentext. " + err.message, channel);
-            }
-        } else {
-            onFailed(err.message, channel);
-        }
+        let extension = randomUrl.split("\/").filter((val, index) => val !== "")[2].split(".")[1];
+        let name = Math.floor(new Date().getTime() / 1000);
+        greentextResult.fileName = `${name}.${extension}`;
+        // let reference: StorageReference = ref(storage, `/jodiostestbot/greentext/${name}.${extension}`);
+
+        console.log(`Getting image from: ${randomUrl}`);
+        axios.get(randomUrl, { responseType: 'arraybuffer' }).then(axiosResult => {
+            greentextResult.buffer = Buffer.from(axiosResult.data, "utf-8");
+            resolve(greentextResult);
+        }).catch(err => reject(err)); // not sure if this catch is necessary
     });
 }
 
